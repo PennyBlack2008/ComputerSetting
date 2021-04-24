bc2ac92216384e4375cec7acf011afa4fd6376dd
PennyBlack2008 <jikang@student.42seoul.kr>
2021-04-24 16:49:02 +0900

    push in seocho

---
+++ setVScode.sh
@@ -0,0 +1,10 @@
+# rm /Users/jikang/Library/Application Support/code/user/settings.json
+# rm /Users/jikang/Library/Application Support/code/user/keybindings.json
+
+cp -i /Users/jikang/Documents/ComputerSetting/settings.json Users/jikang/Library/Application Support/code/user/settings.json
+cp -i /Users/jikang/Documents/ComputerSetting/keybindings.json /Users/jikang/Library/Application Support/code/user/keybindings.json
+
+cp -i /Users/jikang/Documents/ComputerSetting/vimrc /Users/jikang/.vimrc
+cp -i /Users/jikang/Documents/ComputerSetting/zshrc /Users/jikang/.zshrc
+
+cp -i /Users/jikang/.iterm /Users/jikang/iterm
