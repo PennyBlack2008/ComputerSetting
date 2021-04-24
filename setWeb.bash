ff85d9e7eccddf7a0b47e9f1561d1e81a608cc34
PennyBlack2008 <jikang@student.42seoul.kr>
2021-04-24 16:55:45 +0900

    good

--- setWeb.bash
+++ setWeb.bash
@@ -1,4 +1,4 @@
 git clone https://github.com/PennyBlack2008/WebservNote.git
 git clone https://github.com/404-DriverNotFound/200-OK.git
 git clone https://github.com/cclaude42/webserv.git
-
+git clone https://github.com/PennyBlack2008/ComputerSetting.git
