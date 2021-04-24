19a35edc412358b7ec813db1c8366c541900ee13
PennyBlack2008 <jikang@student.42seoul.kr>
2021-04-24 16:48:10 +0900

    update in seocho

---
+++ cluster_setup_chrome.sh
@@ -0,0 +1,18 @@
+#open /Applications/Google\ Chrome.app
+open -a Google\ Chrome.app http://www.cplusplus.com/reference/\
+	https://projects.intra.42.fr/projects/webserv\
+	https://docs.microsoft.com/ko-kr/cpp/cpp/cpp-language-reference?view=msvc-160\
+	https://www.notion.so/Git-5d3f373195754de1a344e972b054aacf\
+	https://www.notion.so/webserv-c6b6cf76a53b4b6f975c3abbe1ba1957\
+	https://github.com/404-DriverNotFound/200-OK\
+	https://docs.popekim.com/ko/coding-standards/cpp\
+	https://www.notion.so/webserver-294afc31380b480db023b26ce48bd6fe\
+	https://developer.mozilla.org/ko/docs/Web/HTTP\
+	https://github.com/l-yohai/webserv#readme\
+	https://github.com/cclaude42/webserv/commits/master?before=4a4b5060e95d6332c93f4cdb6e8dde0d05efcd70+245&branch=master\
+
+open /Users/jikang/Desktop/webserv.pdf
+
+open -a Safari.app https://en.cppreference.com/w/c\
+	https://www.cplusplus.com/reference/clibrary/\
+	https://docs.microsoft.com/ko-kr/cpp/c-language/c-language-reference?view=msvc-160\
